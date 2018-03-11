3361b8a5ea444467e68fa646fb639fee1a712ba5
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-03-11 01:40:55 -0500

    Add support for changing the host and private key used

--- sync.sh
+++ sync.sh
@@ -2,8 +2,13 @@
 
 vivado_bin=/opt/Xilinx/Xilinx/Vivado/2017.2/bin/vivado
 
-rsync -avh -e ssh ./* ubuntu@ec2-54-242-3-234.compute-1.amazonaws.com:~/project_source
+vivado_user=ubuntu
+vivado_hostname=ec2-35-169-204-103.compute-1.amazonaws.com
+vivado_host="$vivado_user@$vivado_hostname"
+server_key=~/.ssh/EGR526.pem
 
-ssh ubuntu@ec2-54-242-3-234.compute-1.amazonaws.com "cd project_source; $vivado_bin -mode batch -source ./gen_bitstream.tcl"
+rsync -avh -e "ssh -i $server_key" ./* $vivado_host:~/project_source
 
-scp ubuntu@ec2-54-242-3-234.compute-1.amazonaws.com:~/project_source/project_output/*.bit ./
+ssh -i $server_key $vivado_host "cd project_source; $vivado_bin -mode batch -source ./gen_bitstream.tcl"
+
+scp -i $server_key $vivado_host:~/project_source/project_output/*.bit ./
