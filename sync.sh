a8e586aeb7df8198a9fe23585f859686dda3138e
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-03-27 21:35:01 -0400

    Exit with message when an error occurs

--- sync.sh
+++ sync.sh
@@ -9,6 +9,22 @@ server_key=~/.ssh/EGR526.pem
 
 rsync -avh -e "ssh -i $server_key" ./* $vivado_host:~/project_source
 
-ssh -i $server_key $vivado_host "cd project_source; $vivado_bin -mode batch -source ./gen_bitstream.tcl"
+if [[ $? -eq 0 ]]; then
+    ssh -i $server_key $vivado_host "cd project_source; $vivado_bin -mode batch -source ./gen_bitstream.tcl"
 
-scp -i $server_key $vivado_host:~/project_source/project_output/*.bit ./
+else
+    echo "rsync failed, not generating bitstream."
+    exit $?;
+
+fi
+
+if [[ $? -eq 0 ]]; then
+    scp -i $server_key $vivado_host:~/project_source/project_output/*.bit ./
+
+else
+    echo "bitstream generation failed, not copying bitstream file."
+    exit $?;
+
+fi
+
+echo "All operations completed successfully."
